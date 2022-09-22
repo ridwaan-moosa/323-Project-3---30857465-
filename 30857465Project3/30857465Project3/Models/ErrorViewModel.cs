4a991630f236890ce460ad6674e1c672ab7c310a
ridwaan <ridwaan91@yahoo.com>
2022-09-22 18:24:44 +0200

    commit 1

---
+++ 30857465Project3/30857465Project3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _30857465Project3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
