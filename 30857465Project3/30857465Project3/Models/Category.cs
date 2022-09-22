d6102b1c319cf6ba1ca91ac831f253e122db40da
ridwaan <ridwaan91@yahoo.com>
2022-09-22 18:31:06 +0200

    scaffold commit

---
+++ 30857465Project3/30857465Project3/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace _30857465Project3.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Device = new HashSet<Device>();
+        }
+
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public virtual ICollection<Device> Device { get; set; }
+    }
+}
---
+++ 30857465Project3/30857465Project3/Models/Device.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace _30857465Project3.Models
+{
+    public partial class Device
+    {
+        public Guid DeviceId { get; set; }
+        public string DeviceName { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid ZoneId { get; set; }
+        public string Status { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual Zone Zone { get; set; }
+    }
+}
